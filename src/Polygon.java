6d85823f2d3214c2c0cf16856d62de6c45fd7699
Tgtg29 <tgtg7871@gmail.com>
2023-02-12 17:56:08 -0800

    Week 1 and 2 Project

--- src/Polygon.java
+++ src/Polygon.java
@@ -2,6 +2,7 @@ public class Polygon {
     private int numSides;
     private double sideLength;
     private String shapeType;
+    private double perimeter;
 
     public Polygon(){
         numSides = 3;
@@ -9,10 +10,36 @@ public class Polygon {
         shapeType = "Triangle";
     }
 
-    public Polygon(int s, int sL, String sT){
-        numSides = s;
+    public Polygon(int s, double sL, String sT){
         sideLength = sL;
-        shapeType = sT;
+        if(s < 3){
+            numSides = 3;
+            if(sideLength != 1.0){
+                sideLength = 1.0;
+            }else{
+                sideLength = sL;
+            }
+        }else{
+            numSides = s;
+        }
+
+        if(sT.equals("line")){
+            shapeType = "Triangle";
+        }else{
+            shapeType = sT;
+        }
+//        numSides = s;
+//        sideLength = sL;
+//        shapeType = sT;
+//        if(numSides < 3){
+//            numSides = 3;
+//        }
+//        if(sideLength < 1.0){
+//            sideLength = 1.0;
+//        }
+//        if(shapeType.equals("Line")){
+//            shapeType = "Triangle";
+//        }
     }
 
     public double getSideLength() {
@@ -25,6 +52,26 @@ public class Polygon {
 
     public String getShapeType(){
         return shapeType;
+    }
+
+    public void setSideLength(double newSideLength){
+        sideLength = newSideLength;
+    }
+
+    public void setNumSides(int newNumSides){
+        numSides = newNumSides;
+    }
+
+    public void setShapeName(String newShapeType){
+        shapeType = newShapeType;
+    }
+
+    public double calculatePerimeter(){
+        return numSides * sideLength;
+    }
+
+    public String toString(){
+       return "Your shape is a " + shapeType + " and it has " + numSides + " sides." + "\nIt has a side length of " + sideLength + "." + "\nIt has a perimeter of " + calculatePerimeter() + " units.";
     }
 
 }
